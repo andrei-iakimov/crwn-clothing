4c155a6ae3cdbe81d9db2bbea98c4a667bc54385
Andrei Iakimov <andrei.iakimov@gmail.com>
2021-10-16 20:16:40 -0400

    Add: Collection Item

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import SHOP_DATA from "./shop.data";
+
+import CollectionsPreview from "../../components/preview-collection-component/preview-collection-component";
+
+class ShopPage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    render(){
+        const {collections} = this.state;
+        
+        return <div className="shop-page">
+            {
+                collections.map(({id, ...collectionProps}) =>(
+                    <CollectionsPreview key={id} {...collectionProps} />
+                ))
+            }
+        </div>
+    }
+}
+
+export default ShopPage;
