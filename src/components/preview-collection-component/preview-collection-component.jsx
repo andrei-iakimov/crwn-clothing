20b56adcdaf010bf9a8a2e988f839d9ef003323b
Andrei Iakimov <andrei.iakimov@gmail.com>
2021-10-18 01:07:46 -0400

    Update shop data and grouped items in card reducer

--- src/components/preview-collection-component/preview-collection-component.jsx
+++ src/components/preview-collection-component/preview-collection-component.jsx
@@ -7,8 +7,8 @@ const CollectionsPreview = ({title, items}) =>(
     <div className='collection-preview'>
         <h1>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {items.filter((item, indx) => indx < 4).map(({id, ...itemProps}) => (
-                <CollectionItem key={id} {...itemProps}/>
+            {items.filter((item, indx) => indx < 4).map((item) => (
+                <CollectionItem key={item.id} item={item}/>
             ))}
         </div>
     </div>
